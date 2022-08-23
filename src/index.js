33daa35e0dde1205dc53c278ac778005b8aa20f1
Olena Honcharuk <gasmin07@gmail.com>
2022-08-24 00:38:57 +0300

    feat: add some logic for count word in text-area

---
+++ src/index.js
@@ -0,0 +1,42 @@
+class WordCounter {
+    constructor(inputText) {
+        this.inputText = inputText;
+        this.inputText.addEventListener('input', () => {
+            this.count();
+        });
+    }
+    count() {
+        let wordStat = this.getWordStat(this.inputText.value.trim());
+        this.emitEvent(wordStat);
+    }
+
+    emitEvent(wordStat) {
+        // Create count event
+        let countEvent = new CustomEvent('count', {
+            bubbles: true,
+            cancelable: true,
+            detail: {
+                wordStat
+            }
+        });
+        // dispatch the count event
+        this.inputText.dispatchEvent(countEvent);
+
+    }
+    getWordStat(str) {
+        let matches = str.match(/\S+/g);
+        return {
+            characters: str.length,
+            words: matches ? matches.length : 0,
+        };
+    }
+}
+const inputText = document.querySelector('#text');
+const statElem = document.querySelector('#stat');
+new WordCounter(inputText);
+const render = (event) => {
+    statElem.innerHTML = `<p>You've written <span class="highlight">${event.detail.wordStat.words} words</span> 
+        and <span class="highlight">${event.detail.wordStat.characters} characters</span>.</p>`;
+}
+
+inputText.addEventListener('count', render);
