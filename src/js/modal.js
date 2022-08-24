fcef9590d00abd10dbe2ff72ce47d94f5aaa703b
Olena Honcharuk <gasmin07@gmail.com>
2022-08-24 16:13:21 +0300

    feat: add modal and function for modal

---
+++ src/js/modal.js
@@ -0,0 +1,32 @@
+function addModal() {
+    const modal = document.querySelector('.modal');
+    const modalContent = document.querySelector('.modal-content');
+    const modalBtn = document.querySelector('.hamburger');
+    const closeBtn = document.querySelector('.close-button');
+
+    console.log(modalBtn);
+
+    function toggleModal() {
+        modal.classList.add('show-modal');
+    }
+    function removeModal() {
+        modal.classList.remove('show-modal');
+    }
+
+    modalBtn.addEventListener('click', toggleModal);
+    closeBtn?.addEventListener('click', removeModal);
+    // document.addEventListener('click', (e) => {
+    //   e.stopPropagation();
+    //   if (modalContent) {
+    //     if (e.target !== modalContent) { removeModal(); }
+    //   }
+    // });
+
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            removeModal();
+        }
+    });
+}
+
+export default addModal;
